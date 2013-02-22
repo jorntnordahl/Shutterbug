64bb67cd50ea9b2cff30a6725c04c2452c16daf0
Jorn Nordahl <jorntnordahl@gmail.com>
2013-02-22 08:41:34 -0800

    working away

--- Shutterbug/StanfordTagFlickrTVC.m
+++ Shutterbug/StanfordTagFlickrTVC.m
@@ -16,23 +16,73 @@
 @implementation StanfordTagFlickrTVC
 
 
--(void) setTags:(NSMutableDictionary *)tags
+/*-(void) setTags:(NSDictionary *)tags
 {
     NSLog(@"Tags %@", tags);
     _tags = tags;
+}*/
+
+-(NSDictionary *)tags
+{
+    if (!_tags)
+    {
+        _tags = [self processPhotosTags];
+    }
+    return _tags;
+}
+
+-(void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSLog(@"View will appear");
+    //self.tags = [self processPhotosTags];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    NSLog(@"View did load");
     self.photos = [FlickrFetcher stanfordPhotos];
-    self.tags = [self processPhotosTags];
 }
 
 -(NSDictionary *) processPhotosTags
 {
     NSMutableDictionary *photoTags = [[NSMutableDictionary alloc]init];
+    
+    // if we have photos to use:
+    if (self.photos)
+    {
+        for (NSDictionary *photoInfo in self.photos)
+        {
+            NSString *photoID = [photoInfo valueForKey:FLICKR_PHOTO_ID];
+            NSString *tags = [photoInfo valueForKey:FLICKR_TAGS];
+            NSArray *tagList = [tags componentsSeparatedByString:@" "];
+            //NSLog(@"Photo: %@", photoID);
+            
+            for (NSString *tag in tagList)
+            {
+                //NSLog(@"Tag %@", tag);
+            
+                if ([photoTags objectForKey:tag] == nil)
+                {
+                    // new tag in list, create a new array for the photo id, and add the tag and photo id array to dictionary
+                    NSMutableArray *photoIDList = [[NSMutableArray alloc] init];
+                    [photoIDList addObject:photoID];
+                    
+                    // now add the list to dictionary:
+                    [photoTags setValue:photoIDList forKey:tag];
+                }
+                else
+                {
+                    // this tag is already in the list, so just get the array out, and add this photo id to that tag
+                    NSMutableArray *photoIDList = [photoTags valueForKey:tag];
+                    [photoIDList addObject:photoID];
+                }
+            }
+        }
+    }
+    
+    NSLog(@"Resulting Dictionary %@", photoTags);
     
     // here, loop over the photos, extracting each tag from the list.
     // the tag will be the key in the photo tags dictionary and the id of the photo will go into the
@@ -44,11 +94,11 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
-    /*
-    NSInteger numberOfPhotos = [self.photos count];
-    NSLog(@"Number %d", numberOfPhotos);
-    return numberOfPhotos;*/
+    //return [self.tags count];
+    
+    NSInteger numberofTags = [self.tags count];
+    NSLog(@"Number of Tags %d", numberofTags);
+    return numberofTags;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -65,6 +115,9 @@
 
 -(NSString *) titleForRow:(NSUInteger) row
 {
+    
+    [self.tags value]
+    
     return @"1";
     //return [self.tags[row][FLICKR_PHOTO_TITLE] description];
 }
