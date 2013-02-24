5c4f12ec71e1c5a55c3fb7aaf552150e4bc9802b
Jorn Nordahl <jorntnordahl@gmail.com>
2013-02-23 21:04:18 -0800

    scroll to smaller size on load of image

--- ImageViewController.m
+++ ImageViewController.m
@@ -6,42 +6,39 @@
 //  Copyright (c) 2013 Jorn Nordahl. All rights reserved.
 //
 
+
 #import "ImageViewController.h"
+
 
 @interface ImageViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
 @property (strong, nonatomic) UIImageView *imageView;
-
 @end
+
 
 @implementation ImageViewController
 
 
--(void) setImageURL:(NSURL *)imageURL
+#define MAX_ZOOM_SCALE 2.0
+
+
+-(void)setImageURL:(NSURL *)imageURL
 {
     _imageURL = imageURL;
     [self resetImage];
 }
 
--(void) resetImage
+
+-(void)resetImage
 {
-    if (self.scrollView)
-    {
-        // clear whatever is thtere
+    if (self.scrollView) {
         self.scrollView.contentSize = CGSizeZero;
         self.imageView.image = nil;
         
-        // go out to the web and get the image:
-        NSLog(@"URL: %@", self.imageURL);
-        NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.imageURL];
         
+        NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.imageURL];
         UIImage *image = [[UIImage alloc] initWithData:imageData];
-        if (image)
-        {
-            // reset the zoom scale every time we get a new image:
-            self.scrollView.zoomScale = 1.0;
-            
+        if (image){
             self.scrollView.contentSize = image.size;
             self.imageView.image = image;
             self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
@@ -49,33 +46,56 @@
     }
 }
 
--(UIImageView *) imageView
+
+- (UIImageView *) imageView
 {
-    if (!_imageView)
-    {
-        _imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
-    }
+    if (!_imageView) _imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
     return _imageView;
+}
+
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
 }
 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	[self.scrollView addSubview:self.imageView];
-    
-    //zooming:
-    self.scrollView.minimumZoomScale = 0.2;
-    self.scrollView.maximumZoomScale = 5.0;
-    
+    [self.scrollView addSubview:self.imageView];
+    self.scrollView.minimumZoomScale = 0.1;
+    self.scrollView.maximumZoomScale = MAX_ZOOM_SCALE;
     self.scrollView.delegate = self;
-    
     [self resetImage];
 }
 
--(UIView *) viewForZoomingInScrollView:(UIScrollView *)scrollView
+
+- (void)viewDidLayoutSubviews
 {
-    return self.imageView;
+    [super viewDidLayoutSubviews];
+    
+    
+    //We need to find the lesser minimum that still shows the whole picture
+    float heightZoomMin = self.scrollView.bounds.size.height / self.imageView.image.size.height;
+    float widthZoomMin  = self.scrollView.bounds.size.width  / self.imageView.image.size.width;
+    //Zoom to the lesser level to see the whole picture
+    self.scrollView.zoomScale = (widthZoomMin > heightZoomMin) ? heightZoomMin : widthZoomMin;
+    //Also set the minimum to the lesser level, so there is only background visible on the botom or side but never both
+    self.scrollView.minimumZoomScale = self.scrollView.zoomScale;
+    
+    
+    //Just in case we get an image with less pixels than our view area, set the minZoom to 1.0
+    if (self.scrollView.minimumZoomScale > MAX_ZOOM_SCALE ) {
+        self.scrollView.minimumZoomScale = 1;
+    }
+    
+    
+    //Log out the important paramaters
+    NSLog(@"viewDidLayoutSubviews - Width: %d, Height: %d, Min Zoom Scale: %f",
+          (int)self.imageView.image.size.width,
+          (int)self.imageView.image.size.height,
+          self.scrollView.minimumZoomScale);
 }
 
 
